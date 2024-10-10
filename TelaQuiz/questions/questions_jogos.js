be536746f83d3a817735cddc2179e6fd8fe52df3
Rafael Luiz <rlg.santos123@gmail.com>
2024-10-10 20:50:26 -0300

    Commit

---
+++ TelaQuiz/questions/questions_jogos.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    question: "Qual foi o ano de lançamento do jogo 'GTA: San Andreas'?",
+    answers: [
+      { option: "2002", correct: false },
+      { option: "2004", correct: true },
+      { option: "2006", correct: false },
+      { option: "2005", correct: false },
+    ],
+  },
+  {
+    question: "Qual console foi lançado em 2000?",
+    answers: [
+      { option: "PlayStation 2", correct: true },
+      { option: "Xbox 360", correct: false },
+      { option: "Nintendo Wii", correct: false },
+      { option: "Dreamcast", correct: false },
+    ],
+  },
+  {
+    question: "Qual jogo popularizou o gênero de tiro em primeira pessoa online nos anos 2000?",
+    answers: [
+      { option: "Counter-Strike", correct: true },
+      { option: "Halo", correct: false },
+      { option: "Call of Duty", correct: false },
+      { option: "Battlefield", correct: false },
+    ],
+  },
+  {
+    question: "Em que ano o jogo 'The Sims' foi lançado?",
+    answers: [
+      { option: "2000", correct: true },
+      { option: "2002", correct: false },
+      { option: "2005", correct: false },
+      { option: "2003", correct: false },
+    ],
+  },
+  {
+    question: "Qual era o nome do personagem principal de 'Prince of Persia: Sands of Time'?",
+    answers: [
+      { option: "Altair", correct: false },
+      { option: "Príncipe", correct: true },
+      { option: "Nathan Drake", correct: false },
+      { option: "Ezio", correct: false },
+    ],
+  },
+  {
+    question: "Qual foi o principal antagonista de 'The Legend of Zelda: The Wind Waker'?",
+    answers: [
+      { option: "Ganon", correct: true },
+      { option: "Majora", correct: false },
+      { option: "Vaati", correct: false },
+      { option: "Zant", correct: false },
+    ],
+  },
+  {
+    question: "Qual jogo foi um dos primeiros a introduzir o gênero 'sandbox' nos anos 2000?",
+    answers: [
+      { option: "Minecraft", correct: true },
+      { option: "Terraria", correct: false },
+      { option: "World of Warcraft", correct: false },
+      { option: "Second Life", correct: false },
+    ],
+  },
+  {
+    question: "Qual jogo de corrida popularizado nos anos 2000 apresentava uma trilha sonora eletrônica e underground?",
+    answers: [
+      { option: "Need for Speed: Underground", correct: true },
+      { option: "Gran Turismo 4", correct: false },
+      { option: "Forza Horizon", correct: false },
+      { option: "Midnight Club 3", correct: false },
+    ],
+  },
+  {
+    question: "Qual desses jogos se tornou um fenômeno de MMORPG nos anos 2000?",
+    answers: [
+      { option: "World of Warcraft", correct: true },
+      { option: "Runescape", correct: false },
+      { option: "Lineage II", correct: false },
+      { option: "Guild Wars", correct: false },
+    ],
+  },
+  {
+    question: "Qual era o nome da inteligência artificial em 'Halo'?",
+    answers: [
+      { option: "Cortana", correct: true },
+      { option: "Alexa", correct: false },
+      { option: "Siri", correct: false },
+      { option: "Delilah", correct: false },
+    ],
+  },
+];
